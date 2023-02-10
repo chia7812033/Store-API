6fe32df1be793092bb57a2e762ac47fbab31d4e0
chia7812033 <dan891221@gmail.com>
2023-02-10 14:47:20 +0800

    Set up some searching funtionality

--- controllers/products.js
+++ controllers/products.js
@@ -1,10 +1,27 @@
+const Product = require('../models/product')
+
 
 const getAllProductsStatic = async (req, res) => {
-  res.status(200).send({msg: "Get all products testing STATIC"})
+  const products = await Product.find({
+    featured: true
+  })
+  res.status(200).json({ products, nbHits: products.length })
 }
 
 const getAllProducts = async (req, res) => {
-  res.status(200).send({msg: "Get all products testing"})
+  const { name: productName, featured: productFeatured, company: productCompany } = req.query
+  const queryObject = {}
+  if (productFeatured) {
+    queryObject.featured = productFeatured === 'true' ? true : false
+  }
+  if (productName) {
+    queryObject.name = { $regex: productName, $options: 'i' }
+  }
+  if (productCompany) {
+    queryObject.company = productCompany
+  }
+  const products = await Product.find(queryObject)
+  res.status(200).json({ products, nbHits: products.length })
 }
 
 module.exports = {
